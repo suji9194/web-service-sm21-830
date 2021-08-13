921d12936ea49db71773901ad711a35ebaba2986
suji9194 <82463876+suji9194@users.noreply.github.com>
2021-08-12 21:02:32 -0700

    Loads initial files

--- index.php
+++ index.php
@@ -58,7 +58,17 @@ function bondJSON(data) {
 	//using this i can see the object in the console
 	console.log(data);
 
-	$('#filmtitle').html(data.);
+	// this defines the type of info returned 
+	$('#filmtitle').html(data.title);
+
+	$.each(data.films,function(i,item){
+		let str = bondTemplate(item);
+
+		$('<div></div>').html(str).appendTo('#films');
+
+	});
+
+
 	// in this way we can see all of the data on the page 
 	/*
 	let myData = JSON.stringify(data,null,4);
